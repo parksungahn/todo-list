c566e93c6d62987ca9e1e0f1a201f4834ffab845
appzzang@gmail.com <appzzang@gmail.com>
2023-03-21 11:24:17 +0900

    init

--- src/todo/TodoMain.jsx
+++ src/todo/TodoMain.jsx
@@ -8,6 +8,24 @@ const TodoMainWrapper = styled('div', {
     padding: 50,
 });
 
+const TodoListItemContent = styled('span', {
+    variants: {
+        status: {
+            doing: {
+
+            },
+            done: {
+                textDecoration: 'line-through',
+                color: '#a0a0a0',
+            },
+        }
+    },
+
+    defaultVariants: {
+        status: 'doing',
+    },
+});
+
 const TodoMain = () => {
 
     // const [todos, setTodos] = useState([
@@ -20,19 +38,33 @@ const TodoMain = () => {
 
     const [todos, setTodos] = useState([]);
 
-    // const addNewTodoItem = (content) => {
-    //     return setTodos(
-    //         (prevState) => {
-    //             return [{id: uuid(), content: content, isDone: false, createdAt: new Date()} , ...prevState];
-    //         }
-    //     );
-    // };
-
     const addNewTodoItem = (content) => {
-         setTodos(
-            (prevState) => [
-                 {id: 'test1', content: 'todo item1', isDone: false, createdAt: new Date()} , ...prevState,]
-            )};
+        return setTodos(
+            (prevState) => {
+                return [{id: uuid(), content: content, isDone: false, createdAt: new Date()} , ...prevState];
+            }
+        );
+    };
+
+    // const addNewTodoItem = (content) => {
+    //      setTodos(
+    //         (prevState) => [
+    //              {id: uuid(), content: content, isDone: false, createdAt: new Date()} , ...prevState,]
+    //         )};
+
+    const toggleTodoItem = (id) => {
+        setTodos(prevTodos => prevTodos.map(it => {
+            if(it.id === id)
+            {
+                //--조건값 변경
+                return{
+                    ...it, isDone: !it.isDone
+                };
+            }
+
+            return it;
+        }))
+    };
 
     return (
         <TodoMainWrapper>
@@ -46,13 +78,14 @@ const TodoMain = () => {
             {
                 todos.map(
                     it => (
-                        <div key={it.id}>
-                            <input type={"checkbox"}/>
-                            <span>{it.content}</span>
+                        <div key={it.id} >
+                            <input type={"checkbox"} value={it.isDone} onChange={ () => toggleTodoItem(it.id)} />
+                            <TodoListItemContent status={it.isDone ? "done" : "doing"}>{it.content}</TodoListItemContent>
                             <button>Delete</button>
                         </div>
                     )
                 )
+
             }
         </TodoMainWrapper>
     );
